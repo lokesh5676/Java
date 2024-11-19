6ae7b4141984977423148b5b801583872a1d6261
lokesh5676 <lokeshlokeshk20@gmail.com>
2024-11-19 10:30:47 +0530

    Add files via upload

---
+++ IcreementDecreement.java
@@ -0,0 +1,14 @@
+
+public class IcreementDecreement {
+
+	public static void main(String[] args) {
+
+		int i=1,b=0;
+		System.out.println("Differents between post and pre increment");
+		
+		System.out.println("preIncrement:"+(++i));
+		System.out.println("postIncrement:"+(b++));
+
+	}
+
+}
